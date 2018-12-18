9032002be34ddc1753dda38bb7de0f9e1fc87eb7
Hannah <hannahvanseeters@gmail.com>
2018-12-17 16:30:33 -0800

    put routers in videostore

--- src/components/VideoStore.js
+++ src/components/VideoStore.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Search from './Search';
+import Customers from './Customers';
+import Library from './Library';
 
 class VideoStore extends Component {
   constructor() {
@@ -15,8 +20,37 @@ class VideoStore extends Component {
   }
 
   render() {
-    return <h2> VideoStore </h2>
+    return (
+    <section>
+      <Router>
+         <div>
+           <ul>
+             <li>
+               <Link to="/">Home</Link>
+             </li>
+             <li>
+               <Link to="/search">Search</Link>
+             </li>
+             <li>
+               <Link to="/customers">Customers</Link>
+             </li>
+             <li>
+               <Link to="/library">Library</Link>
+             </li>
+           </ul>
 
+           <hr />
+
+
+           <Route path="/search" component={Search} />
+           <Route path="/customers" component={Customers} />
+           <Route path="/library" component={Library}/>
+         </div>
+      </Router>
+      <h2> VideoStore </h2>
+
+    </section>
+  )
   }
 }
 
