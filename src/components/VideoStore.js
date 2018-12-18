c9646879971cf46310c3357357d43fa0ac1ddec2
Hannah <hannahvanseeters@gmail.com>
2018-12-18 13:10:35 -0800

    merge

--- src/components/VideoStore.js
+++ src/components/VideoStore.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Search from "./Search";
-import Customers from "./Customers";
-import Library from "./Library";
-import "./VideoStore.css";
+import Search from './Search';
+import Customers from './Customers';
+import Library from './Library';
+import './VideoStore.css'
 
 class VideoStore extends Component {
   constructor() {
     super();
 
     this.state = {
-      customerName: "",
+      customerName: '',
       movieName: "",
       isSubmitted: false,
       customers: [],
-      alertMessage: ""
-    };
+      alertMessage: "",
+    }
   }
 
   addMovieName = (name) => {
@@ -24,45 +24,6 @@ class VideoStore extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <section>
-        <h1 className="text-center"> VideoStore </h1>
-
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/search">Search</Link>
-              </li>
-              <li>
-                <Link to="/customers">Customers</Link>
-              </li>
-              <li>
-                <Link to="/library">Library</Link>
-              </li>
-              <li>
-                Movie Name: <span>{this.state.movieName}</span>
-              </li>
-              <li>
-                Customer Name: <span>{this.state.customerName}</span>
-              </li>
-              <button className="btn btn-info">Submit Rental</button>
-            </ul>
-
-            <Route path="/search" component={Search} />
-            <Route path="/customers" component={Customers} />
-            <Route path="/library" component={Library} />
-          </div>
-        </Router>
-        <div>
-          <Library addMovieNameCallback={this.addMovieName} />
-        </div>
-      </section>
-    );
-=======
     <section>
       <h1 className="text-center"> VideoStore </h1>
 
@@ -104,7 +65,6 @@ class VideoStore extends Component {
 
     </section>
   )
->>>>>>> hannah
   }
 }
 
