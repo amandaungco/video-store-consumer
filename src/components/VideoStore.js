1d71ea3fead5f5f63196e913424f6297719971b7
Hannah <hannahvanseeters@gmail.com>
2018-12-18 13:31:35 -0800

    before merge

--- src/components/VideoStore.js
+++ src/components/VideoStore.js
@@ -13,29 +13,17 @@ class VideoStore extends Component {
       customerName: '',
       movieName: "",
       isSubmitted: false,
-<<<<<<< HEAD
       customers: [],
       alertMessage: "",
     }
-=======
-      customers: ["test"],
-      alertMessage: ""
-    };
->>>>>>> e6259afbd7ac108b1de50383541896dce5d27078
   }
 
-  addCustomerName = name => {
-    this.setState({
-      customerName: name
-    });
-  };
+  addMovieName = (name) => {
+    this.setState({movieName: name})
+  }
 
-  addMovieName = name => {
-    this.setState({ movieName: name });
-  };
   render() {
     return (
-<<<<<<< HEAD
     <section>
       <h1 className="text-center"> VideoStore </h1>
 
@@ -77,58 +65,6 @@ class VideoStore extends Component {
 
     </section>
   )
-=======
-      <section>
-        <h1 className="text-center"> VideoStore </h1>
-
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/search">Search</Link>
-              </li>
-              <li>
-                <Link to="/customers">Customers</Link>
-              </li>
-              <li>
-                <Link to="/library">Library</Link>
-              </li>
-              <li>
-                Movie Name: <span>{this.state.movieName}</span>
-              </li>
-              <li>
-                Customer Name: <span>{this.state.customerName}</span>
-              </li>
-              <button className="btn btn-info">Submit Rental</button>
-            </ul>
-            <Route path="/search" component={Search} />
-            <Route
-              path="/customers"
-              render={() => (
-                <Customers
-                  customers={this.state.customers}
-                  addCustomerCallback={this.addCustomerName}
-                />
-              )}
-              customers={this.state.customers}
-            />
-            <Route
-              path="/library"
-              render={() => (
-                <Library addMovieNameCallback={this.addMovieName} />
-              )}
-            />
-          </div>
-        </Router>
-        <div>
-          <Library addMovieNameCallback={this.addMovieName} />
-        </div>
-      </section>
-    );
->>>>>>> e6259afbd7ac108b1de50383541896dce5d27078
   }
 }
 
