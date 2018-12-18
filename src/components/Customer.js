be7a0a78073e83c88ff47431c9b278a2e359d4b3
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-12-17 16:20:16 -0800

    Began work on customers and customer component

---
+++ src/components/Customer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Customer extends Component {
+  render() {
+    return <h2> Customer </h2>;
+  }
+}
+
+export default Customer;
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,8 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Customer from "./Customer";
 
 class Customers extends Component {
+  constructor() {
+    super();
+    this.state = {
+      customers: []
+    };
+  }
+
+  componentDidMount() {
+    // axios get request here
+  }
   render() {
-    return <h2> Customers </h2>
+    return <h2> Customers </h2>;
   }
 }
 
