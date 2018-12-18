a5023b560f7d32c81e490af9b86d6c55ab300356
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-12-17 17:33:14 -0800

    Created basic array of customer objects

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
+// Customer has name
 class Customer extends Component {
   render() {
-    return <h2> Customer </h2>;
+    return (
+      <div>
+        <h2> Customer </h2>
+        <h2>{this.props.name}</h2>
+        <button
+          type="button"
+          onClick={() => this.props.addCustomerCallback(this.props.name)}
+        >
+          Select for Rental
+        </button>
+      </div>
+    );
   }
 }
+
+Customer.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  addCustomerCallback: PropTypes.func
+};
 
 export default Customer;
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -11,19 +11,62 @@ class Customers extends Component {
     };
   }
 
+  // item.map(customer => {
+  // return (
+  // <Customer name={customer.name} id=customer.id key=customer.key />
+  // )
+  // })
+
   componentDidMount() {
-    // axios get request here
     axios
+      // Deploying?
+      // They could run server on -5000
+      // hardcode the heroku version later / change to localhost during development
       .get("http://localhost:5000/customers")
       .then(response => {
-        console.log(response);
+        // Create array of customer objects
+        // Pass back up to Video Store
+        // Have callback update video store state for customers
+        const customerComponents = response.data.map(customer => {
+          console.log(customer);
+          return (
+            <Customer key={customer.id} id={customer.id} name={customer.name} />
+          );
+        });
+        // console.log(customerComponents);
+        this.setState({
+          customers: customerComponents
+        });
+        console.log("customer in state");
+        console.log(this.state.customers);
       })
       .catch(error => {
         console.log(error);
+        this.setState({
+          errorMessage: error.message
+        });
       });
   }
+  customerCollection = () => {
+    return this.state.customers.map(customer => {
+      return (
+        <Customer
+          key={customer.props.id}
+          id={customer.props.id}
+          name={customer.props.name}
+        />
+      );
+    });
+  };
   render() {
-    return <h2> Customers </h2>;
+    return (
+      <div className="customers">
+        <section className="validation-errors">
+          {this.state.errorMessage}
+        </section>
+        {this.customerCollection()}
+      </div>
+    );
   }
 }
 
