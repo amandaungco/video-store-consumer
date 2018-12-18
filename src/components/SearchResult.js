b7e052bdd215ffbbd7c2d818eff64177642787fb
Amanda Ungco <amanda.ungco@gmail.com>
2018-12-18 15:34:38 -0800

    updated post request

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -6,38 +6,41 @@ import axios from "axios";
 
 const SearchResult = (props) => {
   const {title, releaseDate, overview, imageURL} = props;
-    const onSearchResultSelect =()=> {
+  const onSearchResultSelect =()=> {
 
-      axios.post("http://localhost:5000/movies", props)
-      
-        .then(response => {
-          console.log(response)
-          console.log("request posted")
-        })
-        .catch(error => {
-          console.log(error)
-        })
-      }
+
+    axios.post("http://localhost:5000/movies", props)
+
+    .then(response => {
+      console.log(response)
+      console.log("request posted")
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
   return (
-      <div className="card pet-card">
+    <div className="card pet-card">
 
-        <section className="pet-card--header">
+      <section className="pet-card--header">
 
-          {title} {releaseDate} {imageURL}
-          <button
-            onClick={() => {onSearchResultSelect()}}
-            className="btn btn-primary"
-            >
-            Select
-          </button>
-        </section>
-        <section className="pet-card--body">
-          { overview.length > 128 ? `${overview.substring(0, 128)}...` : overview}
-        </section>
+        {title} {releaseDate} {imageURL}
+        <button
+          onClick={() => {onSearchResultSelect()}}
+          className="btn btn-primary"
+          >
+          Select
+        </button>
+      </section>
+      <section className="pet-card--body">
+        { overview.length > 128 ? `${overview.substring(0, 128)}...` : overview}
+      </section>
 
-      </div>
-    );
-  };
+    </div>
+  );
+}
 
 
 
